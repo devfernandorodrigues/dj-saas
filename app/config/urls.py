1c14d46f8669a43e1dfaca725e5a643022d349f6
Fernando Rodrigues <devfernandorodrigues@gmail.com>
2023-08-02 15:45:46 -0300

    feat: add home page

--- app/config/urls.py
+++ app/config/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path("", include("core.urls")),
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
     path("stripe/", include("djstripe.urls", namespace="djstripe")),
