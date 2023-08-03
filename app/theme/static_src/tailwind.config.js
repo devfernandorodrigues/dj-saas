a11ba148f2b77d99d7897173c5cfe298ddd07736
Fernando Rodrigues <devfernandorodrigues@gmail.com>
2023-08-03 13:14:47 -0300

    chore: add daisyui

--- app/theme/static_src/tailwind.config.js
+++ app/theme/static_src/tailwind.config.js
@@ -15,5 +15,30 @@ module.exports = {
     require("@tailwindcss/line-clamp"),
     require("@tailwindcss/aspect-ratio"),
     require("@tailwindcss/typography"),
+    require("daisyui"),
   ],
+  daisyui: {
+    darkTheme: "dark",
+    base: true,
+    styled: true,
+    utils: true,
+    rtl: false,
+    prefix: "",
+    logs: true,
+    themes: [
+      {
+        mytheme: {
+          primary: "#2563eb",
+          secondary: "#828df8",
+          accent: "#f471b5",
+          neutral: "#1d283a",
+          "base-100": "#ffffff",
+          info: "#3abff8",
+          success: "#2bd4bd",
+          warning: "#f4c152",
+          error: "#fb6f84",
+        },
+      },
+    ],
+  },
 };
